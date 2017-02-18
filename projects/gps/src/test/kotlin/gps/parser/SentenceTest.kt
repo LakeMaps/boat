a6ed3288ce12c0414872d44a4fd25f171098e106
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-02-18 17:49:52 -0330

    Add NMEA 0183 sentence class

---
+++ projects/gps/src/test/kotlin/gps/parser/SentenceTest.kt
@@ -0,0 +1,18 @@
+package gps.parser
+
+import org.junit.Assert
+import org.junit.Test
+
+class SentenceTest {
+    @Test
+    fun sentenceChecksumIsCorrect() {
+        val s = Sentence("GP", "VTG", arrayOf("165.48", "T", "", "M", "0.03", "N", "0.06", "K", "A"))
+        Assert.assertTrue(s.checksum == 54.toChar())
+    }
+
+    @Test
+    fun sentenceStringRepresentationIsCorrect() {
+        val s = Sentence("GP", "VTG", arrayOf("165.48", "T", "", "M", "0.03", "N", "0.06", "K", "A"))
+        Assert.assertEquals(s.toString(), "\$GPVTG,165.48,T,,M,0.03,N,0.06,K,A*36")
+    }
+}
