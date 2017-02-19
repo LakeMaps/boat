1f00e9e395ce6429db0264b32a0111f488df56dc
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-02-19 15:23:18 -0330

    Add Gps class

---
+++ projects/gps/src/main/kotlin/gps/Gps.kt
@@ -0,0 +1,93 @@
+package gps
+
+import gps.parser.Sentence
+import gps.parser.SentenceParser
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.OffsetDateTime
+import java.time.OffsetTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+
+class Gps(recv: () -> Char, private val callback: (Any) -> Unit) {
+    private val parser = SentenceParser(recv)
+
+    fun poll() {
+        val sentence = parser.nextMessage()
+        val type = String(sentence.type)
+        when (type) {
+            "GGA" -> callback(gga(sentence))
+            "GSA" -> callback(gsa(sentence))
+            "GSV" -> callback(gsv(sentence))
+            "RMC" -> callback(rmc(sentence))
+            "VTG" -> callback(vtg(sentence))
+        }
+    }
+
+    internal fun gga(sentence: Sentence): GpsFix {
+        val time = OffsetTime.of(sentence.time(0), ZoneOffset.UTC)
+        val position = GpsPosition(latitude = sentence.double(1), longitude = sentence.double(3))
+        val dOP = GpsDilutionOfPrecision(horizontal = sentence.double(7))
+        return GpsFix(time, position, sentence.char(5), sentence.int(6), dOP, sentence.double(8), sentence.double(10))
+    }
+
+    internal fun gsa(sentence: Sentence): GpsActiveSatellites {
+        val channels = GpsChannelArray((2..15).mapNotNull { sentence.intOrNull(it) }.toIntArray())
+        val dilutionOfPrecision = GpsDilutionOfPrecision(sentence.double(14), sentence.double(15), sentence.double(16))
+        return GpsActiveSatellites(sentence.char(0), sentence.char(1), channels, dilutionOfPrecision)
+    }
+
+    internal fun gsv(sentence: Sentence): GpsSatellitesInView {
+        // These are all conditional for the sake of "symmetry", the first channel should always have an ID
+        val channel1Id = sentence.intOrNull( 3)
+        val channel1 = channel1Id?.let { GpsSatelliteMessage(it, sentence.intOrNull( 4), sentence.intOrNull( 5), sentence.int( 6)) }
+
+        val channel2Id = sentence.intOrNull( 7)
+        val channel2 = channel2Id?.let { GpsSatelliteMessage(it, sentence.intOrNull( 8), sentence.intOrNull( 9), sentence.int(10)) }
+
+        val channel3Id = sentence.intOrNull(11)
+        val channel3 = channel3Id?.let { GpsSatelliteMessage(it, sentence.intOrNull(12), sentence.intOrNull(13), sentence.int(14)) }
+
+        val channel4Id = sentence.intOrNull(15)
+        val channel4 = channel4Id?.let { GpsSatelliteMessage(it, sentence.intOrNull(16), sentence.intOrNull(17), sentence.int(18)) }
+
+        return GpsSatellitesInView(sentence.int(0), sentence.int(1), sentence.int(2), channel1!!, channel2, channel3, channel4)
+    }
+
+    internal fun rmc(sentence: Sentence): GpsNavInfo {
+        val date = sentence.date(8)
+        val time = sentence.time(0)
+        val datetime = OffsetDateTime.of(date, time, ZoneOffset.UTC)
+        val status = sentence.char(1) == GpsNavInfo.STATUS_VALID
+        val position = GpsPosition(sentence.double(4), sentence.double(2))
+        return GpsNavInfo(datetime, status, position, sentence.double(6), sentence.double(7), sentence.char(11))
+    }
+
+    internal fun vtg(sentence: Sentence): GpsGroundVelocity {
+        return GpsGroundVelocity(sentence.double(0), sentence.double(4), sentence.char(8))
+    }
+
+    private fun Sentence.time(index: Int): LocalTime {
+        return LocalTime.parse(String(this.fields[index]), DateTimeFormatter.ofPattern("HHmmss.SSS"))
+    }
+
+    private fun Sentence.date(index: Int): LocalDate {
+        return LocalDate.parse(String(this.fields[index]), DateTimeFormatter.ofPattern("ddMMyy"))
+    }
+
+    private fun Sentence.char(index: Int): Char {
+        return this.fields[index][0]
+    }
+
+    private fun Sentence.intOrNull(index: Int): Int? {
+        return this.fields.getOrNull(index)?.let { String(it).toIntOrNull(radix = 10) }
+    }
+
+    private fun Sentence.int(index: Int): Int {
+        return String(this.fields[index]).toInt(radix = 10)
+    }
+
+    private fun Sentence.double(index: Int): Double {
+        return String(this.fields[index]).toDouble()
+    }
+}
---
+++ projects/gps/src/test/kotlin/gps/GpsTest.kt
@@ -0,0 +1,96 @@
+package gps
+
+import gps.parser.Sentence
+import org.junit.Assert
+import org.junit.Test
+import java.time.OffsetDateTime
+import java.time.OffsetTime
+import java.time.ZoneOffset
+
+class GpsTest {
+    @Test
+    fun testVtgSentence() {
+        val gps = Gps({ '?' }, { })
+        val vtg = gps.vtg(Sentence("GP", "VTG", arrayOf("165.48", "T", "", "M", "0.03", "N", "0.06", "K", "A")))
+
+        Assert.assertEquals(GpsGroundVelocity(165.48, 0.03, 'A'), vtg)
+        Assert.assertTrue("Mode should be Autonomous", vtg.mode == GpsGroundVelocity.MODE_AUTONOMOUS)
+    }
+
+    @Test
+    fun testRmcSentence() {
+        val time = OffsetDateTime.of(2006, 4, 26, 6, 49, 51, 0, ZoneOffset.UTC)
+        val position = GpsPosition(latitude = 2307.1256, longitude = 12016.4438)
+        val expectedGpsNavInfo = GpsNavInfo(time, true, position, 0.03, 165.48, 'A')
+
+        val gps = Gps({ '?' }, { })
+        val rmc = gps.rmc(Sentence("GP", "RMC", arrayOf("064951.000", "A", "2307.1256", "N", "12016.4438", "E", "0.03", "165.48", "260406", "3.05", "W", "A")))
+
+        Assert.assertEquals(expectedGpsNavInfo, rmc)
+        Assert.assertTrue("Mode should be 'Autonomous'", rmc.mode == GpsNavInfo.MODE_AUTONOMOUS)
+        Assert.assertTrue("Status should be 'Valid'", rmc.valid)
+    }
+
+    @Test
+    fun testGsvSentence1() {
+        val channel1 = GpsSatelliteMessage(29, 36,  29, 42)
+        val channel2 = GpsSatelliteMessage(21, 46, 314, 43)
+        val channel3 = GpsSatelliteMessage(26, 44,  20, 43)
+        val channel4 = GpsSatelliteMessage(15, 21, 321, 39)
+        val expectedGsv = GpsSatellitesInView(3, 1, 9, channel1, channel2, channel3, channel4)
+
+        val gps = Gps({ '?' }, { })
+        val gsv = gps.gsv(Sentence("GP", "GSV", arrayOf("3", "1", "09", "29", "36", "029", "42", "21", "46", "314", "43", "26", "44", "020", "43", "15", "21", "321", "39")))
+
+        Assert.assertEquals(expectedGsv, gsv)
+    }
+
+    @Test
+    fun testGsvSentence2() {
+        val channel1 = GpsSatelliteMessage(18, 26, 314, 40)
+        val channel2 = GpsSatelliteMessage( 9, 57, 170, 44)
+        val channel3 = GpsSatelliteMessage( 6, 20, 229, 37)
+        val channel4 = GpsSatelliteMessage(10, 26,  84, 37)
+        val expectedGsv = GpsSatellitesInView(3, 2, 9, channel1, channel2, channel3, channel4)
+
+        val gps = Gps({ '?' }, { })
+        val gsv = gps.gsv(Sentence("GP", "GSV", arrayOf("3", "2", "09", "18", "26", "314", "40", "09", "57", "170", "44", "06", "20", "229", "37", "10", "26", "084", "37")))
+
+        Assert.assertEquals(expectedGsv, gsv)
+    }
+
+    @Test
+    fun testGsvSentence3() {
+        val channel1 = GpsSatelliteMessage(7, null,  null, 26)
+        val expectedGsv = GpsSatellitesInView(3, 3, 9, channel1)
+
+        val gps = Gps({ '?' }, { })
+        val gsv = gps.gsv(Sentence("GP", "GSV", arrayOf("3", "3", "09", "07", "", "", "26")))
+
+        Assert.assertEquals(expectedGsv, gsv)
+    }
+
+    @Test
+    fun testGsaSentence() {
+        val expectedGsa = GpsActiveSatellites('A', '3', GpsChannelArray(intArrayOf(29, 21, 26, 15, 18, 9, 6, 10)), GpsDilutionOfPrecision(2.32, 0.95, 2.11))
+
+        val gps = Gps({ '?' }, { })
+        val gsa = gps.gsa(Sentence("GP", "GSA", arrayOf("A", "3", "29", "21", "26", "15", "18", "09", "06", "10", "", "", "", "", "2.32", "0.95", "2.11")))
+
+        Assert.assertEquals(expectedGsa, gsa)
+        Assert.assertTrue("Mode 1 should be 'Automatic'", gsa.mode1 == GpsActiveSatellites.AUTOMATIC_MODE)
+        Assert.assertTrue("Mode 2 should be '3D (≧4 SVs used)'", gsa.mode2 == GpsActiveSatellites.MODE_3D)
+    }
+
+    @Test
+    fun testGgaSentence() {
+        val time = OffsetTime.of(6, 49, 51, 0, ZoneOffset.UTC)
+        val position = GpsPosition(latitude = 2307.1256, longitude = 12016.4438)
+        val expected = GpsFix(time, position, '1', 8, GpsDilutionOfPrecision(horizontal = 0.95), 39.9, 17.8)
+
+        val gps = Gps({ '?' }, { })
+        val gsa = gps.gga(Sentence("GP", "GGA", arrayOf("064951.000", "2307.1256", "N", "12016.4438", "E", "1", "8", "0.95", "39.9", "M", "17.8", "M", "", "")))
+
+        Assert.assertEquals(expected, gsa)
+    }
+}
