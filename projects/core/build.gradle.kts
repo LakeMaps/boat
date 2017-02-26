ed6127094afbfa7b5fab856363df39cf364d2231
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-02-26 01:28:30 -0330

    Add protobuf Gradle plugin to core

--- projects/core/build.gradle.kts
+++ projects/core/build.gradle.kts
@@ -1,21 +1,35 @@
+import com.google.protobuf.gradle.ProtobufConfigurator
+
+fun addExt(name: String, f: () -> Any) =
+    project.extensions.add(name, f())
+
 buildscript {
     repositories {
         gradleScriptKotlin()
     }
     dependencies {
         classpath(kotlinModule("gradle-plugin"))
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.1")
     }
 }
 
 apply {
     plugin("kotlin")
+    plugin("com.google.protobuf")
 }
 
 dependencies {
     compile(kotlinModule("stdlib"))
     compile(project(":microcontrollers"))
+    compile("com.google.protobuf:protobuf-java:3.2.0")
     compile("org.tinylog:tinylog:1.1")
     compile("rxbroadcast:rxbroadcast:1.1.0")
     testCompile("junit:junit:4.12")
     testCompile("org.jetbrains.kotlin:kotlin-test-junit:1.0.6")
+}
+
+addExt("protobuf") {
+    val protobuf = ProtobufConfigurator(project, null)
+    protobuf.generatedFilesBaseDir = "${projectDir}/src"
+    protobuf
 }
