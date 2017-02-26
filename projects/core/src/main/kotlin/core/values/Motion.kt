950a83e2875087bc4abf95861f5c0c69d584662c
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-02-26 02:13:14 -0330

    Add Motion#encode and Motion#decode functions

--- projects/core/src/main/kotlin/core/values/Motion.kt
+++ projects/core/src/main/kotlin/core/values/Motion.kt
@@ -1,3 +1,20 @@
 package core.values
 
-data class Motion(val surge: Double, val yaw: Double)
+import schemas.MotionProtobuf
+
+data class Motion(val surge: Double, val yaw: Double) {
+    companion object {
+        fun decode(bytes: ByteArray): Motion {
+            val protobuf = MotionProtobuf.Motion.parseFrom(bytes)
+            return Motion(protobuf.surge, protobuf.yaw)
+        }
+    }
+
+    fun encode(): ByteArray {
+        return MotionProtobuf.Motion.newBuilder()
+            .setSurge(surge)
+            .setYaw(yaw)
+            .build()
+            .toByteArray()
+    }
+}
