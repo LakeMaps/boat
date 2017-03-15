1ca6c77db253b738e731134fc0a878dec392a2ff
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-03-14 21:40:20 -0230

    Send bytes into blackhole during microcontroller benchmarks

--- projects/jmh/src/main/kotlin/jmh/MicrocontrollerBenchmark.kt
+++ projects/jmh/src/main/kotlin/jmh/MicrocontrollerBenchmark.kt
@@ -14,16 +14,18 @@ import org.openjdk.jmh.annotations.OutputTimeUnit
 import org.openjdk.jmh.annotations.Scope
 import org.openjdk.jmh.annotations.State
 import org.openjdk.jmh.annotations.Warmup
+import org.openjdk.jmh.infra.Blackhole
 
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.locks.ReentrantLock
 
 class MockSerialPort(
+    private val blackhole: Blackhole,
     private val bytes: ByteArray,
     private var count: Int = 0
 ) {
     fun recv() = bytes[count++]
-    fun send(bytes: ByteArray) { /* empty */ }
+    fun send(bytes: ByteArray) = bytes.forEach { blackhole.consume(it) }
 }
 
 @Fork(1)
@@ -34,36 +36,36 @@ class MockSerialPort(
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 open class MicrocontrollerBenchmark {
     @Benchmark
-    fun resetPropulsionMicrocontroller(): Message {
-        val serial = MockSerialPort(byteArrayOf(0xAA.toByte(), 0x10, 0x00, 0x79, 0x2E))
+    fun resetPropulsionMicrocontroller(blackhole: Blackhole): Message {
+        val serial = MockSerialPort(blackhole, byteArrayOf(0xAA.toByte(), 0x10, 0x00, 0x79, 0x2E))
         val microcontroller = PropulsionMicrocontroller(ReentrantLock(), serial::recv, serial::send)
         return microcontroller.reset()
     }
 
     @Benchmark
-    fun setSpeedPropulsionMicrocontroller(): Message {
-        val serial = MockSerialPort(byteArrayOf(0xAA.toByte(), 0x13, 0x00, 0x80.toByte(), 0x00, 0x00, 0xB6.toByte(), 0xF8.toByte()))
+    fun setSpeedPropulsionMicrocontroller(blackhole: Blackhole): Message {
+        val serial = MockSerialPort(blackhole, byteArrayOf(0xAA.toByte(), 0x13, 0x00, 0x80.toByte(), 0x00, 0x00, 0xB6.toByte(), 0xF8.toByte()))
         val microcontroller = PropulsionMicrocontroller(ReentrantLock(), serial::recv, serial::send)
         return microcontroller.setSpeed(128, 0)
     }
 
     @Benchmark
-    fun resetWirelessLinkMicrocontroller(): Message {
-        val serial = MockSerialPort(byteArrayOf(0xAA.toByte(), 0x00, 0x00, 0x7A, 0x5D))
+    fun resetWirelessLinkMicrocontroller(blackhole: Blackhole): Message {
+        val serial = MockSerialPort(blackhole, byteArrayOf(0xAA.toByte(), 0x00, 0x00, 0x7A, 0x5D))
         val microcontroller = WirelessLinkMicrocontroller(ReentrantLock(), serial::recv, serial::send)
         return microcontroller.reset()
     }
 
     @Benchmark
-    fun receiveWirelessLinkMicrocontroller(): WirelessLinkReceiveMessage {
-        val serial = MockSerialPort(byteArrayOf(0xAA.toByte(), 0x03) + ByteArray(64, { 0x33 }) + byteArrayOf(0x06, 0x9F.toByte()))
+    fun receiveWirelessLinkMicrocontroller(blackhole: Blackhole): WirelessLinkReceiveMessage {
+        val serial = MockSerialPort(blackhole, byteArrayOf(0xAA.toByte(), 0x03) + ByteArray(64, { 0x33 }) + byteArrayOf(0x06, 0x9F.toByte()))
         val microcontroller = WirelessLinkMicrocontroller(ReentrantLock(), serial::recv, serial::send)
         return microcontroller.receive()
     }
 
     @Benchmark
-    fun sendWirelessLinkMicrocontroller(): Message {
-        val serial = MockSerialPort(byteArrayOf(0xAA.toByte(), 0x04, 0x01, 0xA6.toByte(), 0xB8.toByte()))
+    fun sendWirelessLinkMicrocontroller(blackhole: Blackhole): Message {
+        val serial = MockSerialPort(blackhole, byteArrayOf(0xAA.toByte(), 0x04, 0x01, 0xA6.toByte(), 0xB8.toByte()))
         val microcontroller = WirelessLinkMicrocontroller(ReentrantLock(), serial::recv, serial::send)
         return microcontroller.send(ByteArray(61, { 0x42 }))
     }
