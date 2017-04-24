8bf7eaa349cd584a29685166b0d6887b80ec01cc
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-04-24 21:09:23 -0230

    Add angle, length, and speed quantities

---
+++ projects/units/src/main/kotlin/javax/measure/quantity/Angle.kt
@@ -0,0 +1,8 @@
+package javax.measure.quantity
+
+import javax.measure.Quantity
+
+/**
+ * @see <a href="https://en.wikipedia.org/wiki/Angle">Wikipedia: Angle</a>
+ */
+interface Angle: Quantity<Angle>
---
+++ projects/units/src/main/kotlin/javax/measure/quantity/Speed.kt
@@ -0,0 +1,10 @@
+package javax.measure.quantity
+
+import javax.measure.Quantity
+
+/**
+ * Distance traveled divided by the time.
+ *
+ * @see <a href="http://en.wikipedia.org/wiki/Speed">Wikipedia: Speed</a>
+ */
+interface Speed: Quantity<Speed>
