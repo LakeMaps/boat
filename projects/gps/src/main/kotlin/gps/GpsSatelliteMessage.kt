0dac5776a451dab7c8770e85693b285b41911f2e
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-05-01 23:25:50 -0230

    Update GPS types to store quantities

--- projects/gps/src/main/kotlin/gps/GpsSatelliteMessage.kt
+++ projects/gps/src/main/kotlin/gps/GpsSatelliteMessage.kt
@@ -1,11 +1,15 @@
 package gps
 
+import units.Angle
+import units.Degree
+import units.Quantity
+
 /**
  * A satellite message.
  *
  * @property id the satellite ID (from 1 to 32)
- * @property elevation the elevation in degrees
- * @property azimuth the azimuth measurement in degrees
+ * @property elevation the elevation
+ * @property azimuth the azimuth
  * @property signalRatio the signal-to-noise ratio in dBHz
  */
-data class GpsSatelliteMessage(val id: Int, val elevation: Int?, val azimuth: Int?, val signalRatio: Int?)
+data class GpsSatelliteMessage(val id: Int, val elevation: Quantity<Angle, Degree>?, val azimuth: Quantity<Angle, Degree>?, val signalRatio: Int?)
