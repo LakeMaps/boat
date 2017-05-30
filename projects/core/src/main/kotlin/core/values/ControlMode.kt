1c17410025e3346043e82ead2e711ebbfd309bf3
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-05-30 00:13:03 -0230

    Add ControlMode.decode to decode from TypedMessage

--- projects/core/src/main/kotlin/core/values/ControlMode.kt
+++ projects/core/src/main/kotlin/core/values/ControlMode.kt
@@ -1,6 +1,20 @@
 package core.values
 
+import schemas.ControlModeProtobuf
+import schemas.TypedMessageProtobuf
+
 sealed class ControlMode {
+    companion object {
+        fun decode(bytes: ByteArray): ControlMode {
+            val obj = TypedMessageProtobuf.TypedMessage.parseFrom(bytes)
+            val controlMode = obj.controlMode
+            return when (controlMode.mode!!) {
+                ControlModeProtobuf.ControlMode.Mode.MANUAL -> MANUAL
+                ControlModeProtobuf.ControlMode.Mode.WAYPOINT -> WAYPOINT
+            }
+        }
+    }
+
     object MANUAL: ControlMode()
     object WAYPOINT: ControlMode()
 }
