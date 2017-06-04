03573a980d68ffb5163fe2f43e6af716fc9f5bfa
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-06-04 12:35:16 -0230

    Add another test for ControlSystemTransformer<T>

--- projects/core/src/test/kotlin/core/control/ControlSystemTransformerTest.kt
+++ projects/core/src/test/kotlin/core/control/ControlSystemTransformerTest.kt
@@ -225,4 +225,69 @@ class ControlSystemTransformerTest {
         testSubscriber.assertCompleted()
         testSubscriber.assertReceivedOnNext(listOf(0.0, 15.0, 18.0, 26.0, 35.0, 40.0, 15.0, 17.0, 12.0, 12.0, 10.0))
     }
+
+    @Test
+    fun testSystem3() {
+        val e = { _: Double, value: Double -> value }
+        val controller = PidController(PidController.Gains(kp = 2.0, ki = 1.0, kd = 8.0), { x: Double -> x }, ti = 32)
+        val testScheduler = TestScheduler()
+        val testSubject = TestSubject.create<Double>(testScheduler)
+        val testSubscriber = TestSubscriber<Double>()
+
+        ControlSystemTransformer(0.0, Observable.just(ControlSystem(controller, e)), testScheduler)
+            .call(testSubject)
+            .subscribe(testSubscriber)
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MILLISECONDS)
+        testSubject.onNext(0.0)
+        testScheduler.triggerActions()
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MILLISECONDS)
+        testSubject.onNext(9.0)
+        testScheduler.triggerActions()
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MILLISECONDS)
+        testSubject.onNext(8.0)
+        testScheduler.triggerActions()
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MILLISECONDS)
+        testSubject.onNext(7.0)
+        testScheduler.triggerActions()
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MILLISECONDS)
+        testSubject.onNext(6.0)
+        testScheduler.triggerActions()
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MILLISECONDS)
+        testSubject.onNext(5.0)
+        testScheduler.triggerActions()
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MILLISECONDS)
+        testSubject.onNext(4.0)
+        testScheduler.triggerActions()
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MILLISECONDS)
+        testSubject.onNext(3.0)
+        testScheduler.triggerActions()
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MILLISECONDS)
+        testSubject.onNext(2.0)
+        testScheduler.triggerActions()
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MILLISECONDS)
+        testSubject.onNext(1.0)
+        testScheduler.triggerActions()
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MILLISECONDS)
+        testSubject.onNext(0.0)
+        testScheduler.triggerActions()
+
+        testSubject.onCompleted()
+        testScheduler.triggerActions()
+
+        testSubscriber.awaitTerminalEvent(TIMEOUT_MS, TimeUnit.MILLISECONDS)
+        testSubscriber.assertNoErrors()
+        testSubscriber.assertCompleted()
+        testSubscriber.assertReceivedOnNext(listOf(0.0, 99.0, 25.0, 30.0, 34.0, 37.0, 39.0, 40.0, 40.0, 39.0, 37.0))
+    }
 }
