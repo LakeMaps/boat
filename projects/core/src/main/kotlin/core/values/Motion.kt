0b9356ee8b121896d01a5bb11f777645a4cac72f
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-06-11 00:07:11 -0230

    Add Motion.ZERO constant

--- projects/core/src/main/kotlin/core/values/Motion.kt
+++ projects/core/src/main/kotlin/core/values/Motion.kt
@@ -2,6 +2,8 @@ package core.values
 
 data class Motion(val surge: Double, val yaw: Double) {
     companion object {
+        val ZERO = Motion(0.0, 0.0)
+
         fun clamp(value: Double) = when {
             value >  1.0 ->  1.0
             value < -1.0 -> -1.0
