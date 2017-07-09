391ad519e28fe14cd3dd2317b9b24cd60f0aaf62
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-07-08 23:26:18 -0230

    Add rocksdb project

---
+++ projects/rocksdb/build.gradle.kts
@@ -0,0 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+repositories {
+    jcenter()
+}
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.1.3"
+}
+
+dependencies {
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8")
+    compile("org.rocksdb:rocksdbjni:5.5.1")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
