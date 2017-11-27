d0e285338fe36a7a028e2b2f5937fca8661535b2
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2017-11-26 21:29:27 -0330

    Update projects to Kotlin 1.1.61

--- projects/core/build.gradle.kts
+++ projects/core/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.1.4"
+    id("org.jetbrains.kotlin.jvm") version "1.1.61"
     id("com.google.protobuf") version "0.8.1"
 }
 
