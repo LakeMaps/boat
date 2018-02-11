c2881eb78e66055065514f96e55d14f741e02244
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2018-02-10 21:36:41 -0330

    Add sendAsync method to Broadcast wrapper

--- projects/core/src/main/kotlin/core/broadcast/Broadcast.kt
+++ projects/core/src/main/kotlin/core/broadcast/Broadcast.kt
@@ -4,3 +4,7 @@ import rx.Observable
 import rx.broadcast.Broadcast
 
 inline fun <reified T : Any> Broadcast.valuesOfType(): Observable<T> = valuesOfType(T::class.java)
+
+inline fun <reified T : Any> Broadcast.sendAsync(value: T) {
+    send(value).subscribe()
+}
