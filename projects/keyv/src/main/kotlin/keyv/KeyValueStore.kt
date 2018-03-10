9107fa5a1d02f5a211ac7dfc87395a5535b248f2
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2018-03-10 16:03:31 +0100

    Rewrite Rocks class to include namespaces + add interface

---
+++ projects/keyv/src/main/kotlin/keyv/KeyValueStore.kt
@@ -0,0 +1,47 @@
+package keyv
+
+interface KeyValueStore {
+    /**
+     * Set the value for the given key.
+     *
+     * @param key the specified key to be inserted
+     * @param value the value associated with the specified key
+     *
+     * @throws Exception if the put cannot be completed.
+     */
+    fun put(key: ByteArray, value: ByteArray)
+
+    /**
+     * Set the value for the given key.
+     *
+     * The given string is assumed to be UTF 8.
+     *
+     * @param key the specified key to be inserted
+     * @param value the value associated with the specified key
+     *
+     * @throws Exception if the put cannot be completed.
+     */
+    fun put(key: ByteArray, value: String) = put(key, value.toByteArray(Charsets.UTF_8))
+
+    /**
+     * Returns a new {@code ByteArray} storing the value associated for the given key.
+     *
+     * If the given key does not have a value associated with it {@code null} will be
+     * returned.
+     *
+     * @param key the key retrieve the value
+     * @returns a {@code ByteArray} storing the value associated for the given key or {@code null}
+     *
+     * @throws Exception if the get cannot be completed
+     */
+    fun get(key: ByteArray): ByteArray?
+
+    /**
+     * Deletes the entry (if any) for the given key.
+     *
+     * @param key the key to delete within database
+     *
+     * @throws Exception if the delete cannot be completed
+     */
+    fun delete(key: ByteArray)
+}
